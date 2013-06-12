2a5fec73f8a6cd973d0fc1ca565b06b20489a403
hlb <hlb@handlino.com>
2013-06-12 17:17:25 +0800

    add base64_encode

--- lib/random_color.rb
+++ lib/random_color.rb
@@ -2,7 +2,7 @@
 # origin: http://victorcoulon.fr/generating-random-color-in-sass/
 
 module Sass::Script::Functions
-  def random-color()
+  def random_color()
     Sass::Script::Color.new([rand(255), rand(255), rand(255)])
   end
 end
